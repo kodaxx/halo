5e1609a7d4092dc1937af71bb694e2a3a19203b5
Spencer Kuzara <spencerkuzara@Spencers-MacBook-Pro.local>
2026-01-16 17:05:48 +0900

    Remove deprecated scripts and create new dir structure

--- setup_mesh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# setup_mesh.sh
-# Installs dependencies for Halo Mesh (batman-adv, etc)
-
-echo "Installing Mesh Dependencies..."
-sudo sudo apt-get update
-sudo sudo apt-get install -y batctl dnsmasq iptables bridge-utils python3-flask
-
-echo "Dependencies Installed."
