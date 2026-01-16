9014e680e7f45322dd105d738eab5ba7ad4c2198
Spencer Kuzara <spencerkuzara@Spencers-MacBook-Pro.local>
2026-01-16 23:49:16 +0900

    Fix capture_image.sh: Switch to cheyne/pishrink (User suggestion)

--- dev_tools/capture_image.sh
+++ dev_tools/capture_image.sh
@@ -48,8 +48,8 @@ if command -v docker &> /dev/null; then
     # Volume mount current dir to /workdir
     docker run --privileged=true --rm \
         -v "$(pwd):/workdir" \
-        drewsif/pishrink \
-        pishrink -s "/workdir/$IMAGE_NAME"
+        cheyne/pishrink \
+        pishrink.sh -s "/workdir/$IMAGE_NAME"
         
     echo "PiShrink Complete."
 else
